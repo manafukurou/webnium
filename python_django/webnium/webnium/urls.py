ac6be27990d128a0c31d271ba17e1190c4ee1d70
Tatsuya Koyama <tatsuyakoyama@TatsuyanoMacBook-Pro.local>
2020-10-23 20:42:32 +0900

    test

--- python_django/webnium/webnium/urls.py
+++ python_django/webnium/webnium/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import include,path
 
 urlpatterns = [
-    path('home/', include('home.urls')),
-    path('admin/', admin.site.urls),
+    url('home/', include('home.urls')),
+    url('admin/', admin.site.urls),
 ]
