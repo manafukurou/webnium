46a1f24745d0b078da91ef49c7f513fbc3d8e5c0
Tatsuya Koyama <tatsuyakoyama@TatsuyanoMacBook-Pro.local>
2020-11-03 13:41:44 +0900

    pagenation

--- python_django/webnium/job/models.py
+++ python_django/webnium/job/models.py
@@ -6,3 +6,5 @@ class Job(models.Model):
     job_name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.name
--- python_django/webnium/job/views.py
+++ python_django/webnium/job/views.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from job.models import Job
 
 # Create your views here.
 def list(request):
-    jobData = Job.objects.get(id=3)
-    message = jobData.job_name
-    print(message)
+
+    job_list = Job.objects.order_by('id').reverse()
+    job_paginator = Paginator(job_list, 5)
+
+    page = request.GET.get('page', 1)
+
+    try:
+        jobs = job_paginator.page(page)
+    except PageNotAnInteger:
+        jobs = job_paginator.page(1)
+    except EmptyPage:
+        jobs = job_paginator.page(1)
+
     params = {
-        'message': message,
+        'message': "message",
+        'jobs': jobs
     }
     return render(request, 'list.html', params)
 
 def detail(request):
-    message = "ディティール"
+
+    jobData = Job.objects.get(id=3)
+    message = jobData.job_name
     params = {
         'message': message,
     }
     return render(request, 'detail.html', params)
+
