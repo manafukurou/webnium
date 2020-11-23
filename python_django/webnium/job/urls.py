743b139c98dc5d5f8169cd719c8f3b7850d34fbe
Tatsuya Koyama <tatsuyakoyama@TatsuyanoMacBook-Pro.local>
2020-11-23 12:59:18 +0900

    crad

--- python_django/webnium/job/urls.py
+++ python_django/webnium/job/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('detail/', views.detail, name='detail'),
     path('new_job/', views.new_job, name='new_job'),
     path('confirm/', views.confirm, name='confirm'),
-    path('edit/<int:job_id>', views.edit, name='edit')
+    path('edit/<int:job_id>', views.edit, name='edit'),
+    path('delete/<int:job_id>', views.delete, name='delete'),
+    path('update/<int:job_id>', views.update, name='update')
 ]
--- python_django/webnium/job/views.py
+++ python_django/webnium/job/views.py
@@ -4,11 +4,11 @@ from job.models import Job
 from .forms import JobForm
 import datetime
 
+
 # Create your views here.
 def list(request):
-
     job_list = Job.objects.all()
-    job_paginator = Paginator(job_list, 5)
+    job_paginator = Paginator(job_list, 10)
 
     page = request.GET.get('page', 1)
 
@@ -25,8 +25,8 @@ def list(request):
     }
     return render(request, 'list.html', params)
 
-def detail(request):
 
+def detail(request):
     jobData = Job.objects.get(id=3)
 
     params = {
@@ -34,12 +34,12 @@ def detail(request):
     }
     return render(request, 'detail.html', params)
 
-def new_job(request):
 
+def new_job(request):
     initial_dict = dict(
         job_name=""
     )
-    job_form = JobForm(request.GET or None,initial=initial_dict)
+    job_form = JobForm(request.GET or None, initial=initial_dict)
 
     params = {
         'message': "edit",
@@ -48,18 +48,58 @@ def new_job(request):
     return render(request, 'new_job.html', params)
 
 
-def edit(request,job_id):
+def update(request, job_id):
+    job_job = Job.objects.all().filter(id=job_id).first()
+    initial_dict = dict(
+        job_name=job_job.job_name,
+        job_id=job_job.id,
+    )
+    job_form = JobForm(request.GET or None, initial=initial_dict)
+
+    params = {
+        'message': "edit",
+        'job_form': job_form
+    }
+    return render(request, 'new_job.html', params)
+
+
+def edit(request, job_id):
+    job_job = Job.objects.all().filter(id=job_id).first()
+
     context = {
-        'job_id': job_id
+        'job_id': job_id,
+        'job_job': job_job
     }
     return render(request, 'edit.html', context)
 
-    return "";
 
-def confirm( request, *args, **kwargs):
-    jobObject = Job(job_name=request.POST['job_name'],pub_date=datetime.datetime.now())
-    jobObject.save()
+def delete(request, job_id):
+    # job_job = Job.objects.all().filter(id=job_id).first()
+    Job.objects.filter(id=job_id).delete()
     context = {
-        'job_name': request.POST['job_name']
+        'result': True
+    }
+    return render(request, 'delete.html', context)
+
+
+def confirm(request, *args, **kwargs):
+    job_name = request.POST['job_name']
+    job_id = request.POST['job_id']
+    if job_id is None or job_id == "":
+        job_object = Job(
+            job_name=job_name,
+            pub_date=datetime.datetime.now()
+        )
+        job_object.save()
+        result = "created"
+    else:
+        job_object = Job.objects.all().filter(id=job_id).first()
+        job_object.job_name = job_name
+        job_object.save()
+        result = "updated"
+
+    context = {
+        'job_name': job_name,
+        'result': result
     }
     return render(request, 'confirm.html', context)
