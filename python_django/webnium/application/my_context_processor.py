dda055932ddeaebdfe95d821b335330a494c6ae6
Tatsuya Koyama <tatsuyakoyama@TatsuyanoMacBook-Pro.local>
2020-11-30 20:48:31 +0900

    template js/css

--- python_django/webnium/application/my_context_processor.py
+++ python_django/webnium/application/my_context_processor.py
@@ -1,2 +1,15 @@
 def common(request):
-  return {"title":"ttt"}
+  javascript_list = {
+    "parts/parts.js",
+    "application/Sortable.js",
+    "application/jquery-3.5.1.min.js"
+  }
+  css_list = {
+    "application/bootstrap.min.css"
+  }
+  context = {
+      'title': "webnium",
+      'javascript_list':  javascript_list,
+      'css_list':  css_list,
+  }
+  return context
